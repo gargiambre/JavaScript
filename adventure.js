2f4a561b95430ee3235fd011a6cf92c113db0bd4
GargiAmbre <157484050+gargiambre@users.noreply.github.com>
2024-06-15 22:58:17 +0530

    Update adventure.js

--- adventure.js
+++ adventure.js
@@ -1,65 +1,34 @@
-// const prompt = require("prompt-sync")();
+const prompt = require("prompt-sync")();
 
-// const answer = prompt("Would you like to play (y/n)? ");
+const answer = prompt("Would you like to play (y/n)? ");
 
-// if (answer.toLowerCase() === "y") 
-// {
-//     const answer2 = prompt("Would you like to go left or go right (left/right)? ");
-
-//     if (answer2 === "left") 
-//     {
-//         console.log("Oops, you go left and fall of a bridge! You lost!");
-//     } 
-//     else 
-//     {
-//         // right
-//         console.log("Nice! You go right and approach a bridge...");
-//         const answer3 = prompt("Would you like to cross the bridge, or turn around and find another way (cross/turn)? ");
-
-//         if (answer3 === "cross") 
-//         {
-//             console.log("You cross the bridge and reach the end of the game!");
-//         } 
-//         else 
-//         {
-//             console.log("You turn back and trip on a log and hurt your leg. You lose.");
-//         }
-//     }
-// } 
-// else 
-// {
-//   console.log("That's too bad!");
-// }
-
-
-
-const prompt =  require("prompt-sync")();
-
-const a = prompt("would you like to play the game (y/n)?")
-
-if(a === "y")
+if (answer.toLowerCase() === "y") 
 {
-    const b = prompt("left or right?")
-    if(b === "left")
+    const answer2 = prompt("Would you like to go left or go right (left/right)? ");
+
+    if (answer2 === "left") 
     {
-        console.log("oops you died");
-        //continue;
-    }
-    else
+        console.log("Oops, you go left and fall of a bridge! You lost!");
+    } 
+    else 
     {
-        const d = prompt("cross or turn?")
-        
-        if(d === "cross")
+        console.log("Nice! You go right and approach a bridge...");
+        const answer3 = prompt("Would you like to cross the bridge, or turn around and find another way (cross/turn)? ");
+
+        if (answer3 === "cross") 
         {
-            console.log("you won");
-        }
-        else
+            console.log("You cross the bridge and reach the end of the game!");
+        } 
+        else 
         {
-            console.log("try again");
+            console.log("You turn back and trip on a log and hurt your leg. You lose.");
         }
     }
-}
-else
+} 
+else 
 {
-    console.log("untill next time");
+  console.log("That's too bad!");
 }
+
+
+
